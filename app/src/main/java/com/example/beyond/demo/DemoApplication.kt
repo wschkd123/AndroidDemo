a12a08249778eee8a8948ea0aa666a84c432f639
p_wshicwang <p_wshicwang@yuewen.com>
2024-03-12 18:59:00 +0800

    fix：修复WorkManager更新小部件时一直刷新

--- app/src/main/java/com/example/beyond/demo/DemoApplication.kt
+++ app/src/main/java/com/example/beyond/demo/DemoApplication.kt
@@ -1,7 +1,12 @@
 package com.example.beyond.demo
 
 import android.app.Application
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.beyond.demo.appwidget.CharacterWorker
 import com.example.beyond.demo.common.Init
+import java.util.concurrent.TimeUnit
 
 /**
  *
@@ -12,5 +17,15 @@ class DemoApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         Init.setApplication(this)
+        // 修复WorkManager更新小部件时一直刷新 https://issuetracker.google.com/issues/241076154
+        WorkManager.getInstance(applicationContext)
+            .enqueueUniqueWork(
+                "not_executed_work",
+                ExistingWorkPolicy.KEEP,
+                OneTimeWorkRequest.Builder(CharacterWorker::class.java)
+                    .setInitialDelay(365 * 10, TimeUnit.DAYS)
+                    .build()
+            )
+
     }
 }
